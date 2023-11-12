bcc12ac58cb9470d7ba02ea77311120f98ca632e
NetMaster1 <BiZon-NN@yandex.ru>
2023-11-12 20:32:16 +0000

    remote

--- jobs/jobs.py
+++ jobs/jobs.py
@@ -7,7 +7,7 @@ def scheduled_dispatch():
  #=============================Smsc API=======================
 	#В сообщении нужно обязательно указать отправителя, иначе спам фильтр не пропустит его.
 	#phone=item.phone
-	phone='79200711112'
+	phone=79200711112
 	#message=f'ООО Ритейл. Телефон {item.brand} {item.model} IMEI {item.imei} готов и завтра будет доставлен на точку.'
 	message='Ваш телефон готов'
 	base_url="https://smsc.ru/sys/send.php?login=NetMaster&psw=ylhio65v&phones={}&mes=OOO Ритейл. Ваш телефон готов."

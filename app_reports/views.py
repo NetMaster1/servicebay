16a7faece89b0bc40d4987eeb37ed836c4f93c37
Sergey Vinokurov <79200711112@yandex.ru>
2023-11-12 23:29:56 +0300

    Revert "added status report"
    This reverts commit ebec4c3c85e7b16655bbfe9d659a5504d3fb4e98.

--- app_reports/views.py
+++ app_reports/views.py
@@ -17,9 +17,7 @@ from twilio.rest import Client
 from django.forms.fields import DateField
 import serial
 import time
-from datetime import datetime, date, timedelta
 from app_items.models import Item, Registry, RegistryLine, Status_change
-import xlwt
 
 # Create your views here.
 
@@ -28,52 +26,7 @@ def workshop_reports (request):
     if request.user.is_authenticated:
         items=Item.objects.all()
         if request.method == "POST":
-            # start_date = request.POST["start_date"]
-            # start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
-            # end_date = request.POST["end_date"]
-            # end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
-            # end_date = end_date + timedelta(days=1)
-            status = request.POST["status"]
-            queryset=Item.objects.filter(status=status)
-
-             #==========================Convert to Excel module=========================================
-            response = HttpResponse(content_type="application/ms-excel")
-            response["Content-Disposition"] = (
-                "attachment; filename=SaleRep_" + str(date) + ".xls"
-            )
-
-            # str(datetime.date.today())+'.xls'
-
-            wb = xlwt.Workbook(encoding="utf-8")
-            ws = wb.add_sheet('Statuses')
-
-            # sheet header in the first row
-            row_num = 0
-            font_style = xlwt.XFStyle()
-            columns = ['Дата создания', "Дата изменения статуса", 'IMEI', 'Модель', 'Статус']
-            for col_num in range(len(columns)):
-                ws.write(row_num, col_num + 1, columns[col_num], font_style)
-
-            # sheet body, remaining rows
-            font_style = xlwt.XFStyle()
-
-            row_num = 1
-            for item in queryset:
-                col_num = 1
-                ws.write(row_num, col_num, str(item.created), font_style)
-                col_num +=1
-                ws.write(row_num, col_num, str(item.status_updated), font_style)
-                col_num +=1
-                ws.write(row_num, col_num, item.imei, font_style)
-                col_num +=1
-                ws.write(row_num, col_num, item.model, font_style)
-                col_num +=1
-                ws.write(row_num, col_num, item.status, font_style)
-                row_num +=1
-
-            wb.save(response)
-            return response
-
+            pass
         else:
             return render (request, 'reports/workshop_reports.html')
 
